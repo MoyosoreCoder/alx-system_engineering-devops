221ea14de0729b9101020ad579cbbacfcd8db742
Mariam <kelanimaryam1234@gmail.com>
2024-05-07 20:06:58 -0700

    import position adjusted

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and prints the titles of the
+first 10 hot posts listed for a given subreddit.
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+   """Prototype that return a redirect to search results"""
+
