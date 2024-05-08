7c785634fa66b9fe3830fd5a9760f93c89d8c54a
Mariam <kelanimaryam1234@gmail.com>
2024-05-07 21:07:24 -0700

    new 2-recurse.py file

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,11 +4,7 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """
-    Function that queries the Reddit API and returns a
-    list containing the titles of all hot articles for a given subreddit.
-    If no results are found for the given subreddit, the function should return None.
-    """
+    """Function that queries the Reddit API and returns a list"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
