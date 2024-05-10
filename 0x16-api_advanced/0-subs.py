1220db866749e9da7e91e2c6b8d49929eadcac32
Mariam <kelanimaryam1234@gmail.com>
2024-05-09 22:20:23 -0700

    0-subs.py file added

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
 """
-A a function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit. If an invalid
-subreddit is given, the function should return 0.
+Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    A method that checks if the argument passed is not a valid
-    reddit subscriber
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = 'User Agent'
-    headers = {
-        'User-Agent': user_agent
-    }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        result = response.json().get('data')
-        return result.get('subscribers')
+    """A route that returns the number of subscribers"""
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
         return 0
---
+++ 0x16-api_advanced/new_sub.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A a function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    A method that checks if the argument passed is not a valid
+    reddit subscriber
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    user_agent = 'User Agent'
+    headers = {
+        'User-Agent': user_agent
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        result = response.json().get('data')
+        return result.get('subscribers')
+    else:
+        return 0
